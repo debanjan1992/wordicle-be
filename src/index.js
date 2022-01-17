8c67b86b386557873603b41e90c980821b48e712
Debanjan Saha <debanjansaha1992@gmail.com>
2022-01-17 13:17:41 +0530

    base code

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const PORT = 4000;
+const GameEngine = require("./services/gameEngine");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/api/word", (req, res) => {
+    const sessionId = req.query.sessionId;
+    if (sessionId !== "null") {
+        GameEngine.clearSession(sessionId);
+    }
+    res.json(GameEngine.getRandomWord());
+});
+
+app.get("/api/session/valid", (req, res) => {
+    res.json({ valid: GameEngine.isSessionValid(req.query.id) });
+});
+
+app.get("/api/clear", (req, res) => {
+    GameEngine.clearAllSessions();
+    res.json({ success: true });
+});
+
+app.post("/api/submit", (req, res) => {
+    const answer = req.body.word;
+    const sessionId = req.body.sessionId;
+    GameEngine.submitAnswer(sessionId, answer).then(response => {
+        res.json({ success: true, data: response, word: answer });
+    }).catch(error => {
+        res.status(400).json({ success: false, message: "Not found!" });
+    })
+});
+
+app.listen(PORT, () => `Server running on port ${PORT}`);
---
+++ src/services/gameEngine.js
@@ -0,0 +1,108 @@
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+const { nanoid } = require("nanoid");
+
+class GameEngine {
+
+    static isDemoMode = true;
+    static SESSIONS_FILE_PATH = path.join(__dirname, "../", "data", "sessions.json");
+    static WORDS_FILE_PATH = path.join(__dirname, "../", "data", "words.json");
+
+    static generateRandomId() {
+        return nanoid();
+    }
+
+    static getRandomWord() {
+        if (this.isDemoMode) {
+            return this.createNewSession("CACTUS");
+        } else {
+            const dataset = JSON.parse(fs.readFileSync(this.WORDS_FILE_PATH));
+            const index = Math.floor(Math.random() * dataset.length);
+            const word = dataset[index].word.value;
+            return this.createNewSession(word);
+        }
+    }
+
+    static clearSession(sessionId) {
+        const sessions = JSON.parse(fs.readFileSync(this.SESSIONS_FILE_PATH));
+        delete sessions[sessionId];
+        fs.writeFileSync(this.SESSIONS_FILE_PATH, JSON.stringify({}, null, 4));
+    }
+
+    static clearAllSessions() {
+        fs.writeFileSync(this.SESSIONS_FILE_PATH, JSON.stringify({}, null, 4));
+    }
+
+    static isSessionValid(id) {
+        const sessions = JSON.parse(fs.readFileSync(this.SESSIONS_FILE_PATH));
+        if (!sessions[id]) {
+            return false;
+        }
+        return true;
+    }
+
+    static createNewSession(word) {
+        const id = this.generateRandomId();
+        const sessions = JSON.parse(fs.readFileSync(this.SESSIONS_FILE_PATH));
+        if (!sessions[id]) {
+            sessions[id] = word;
+        }
+        fs.writeFileSync(this.SESSIONS_FILE_PATH, JSON.stringify(sessions, null, 4));
+        return {
+            id: id,
+            length: word.length
+        };
+    }
+
+    static getWordFromSession(sessionId) {
+        const sessions = JSON.parse(fs.readFileSync(this.SESSIONS_FILE_PATH));
+        if (sessions[sessionId]) {
+            return sessions[sessionId].toUpperCase();
+        } else {
+            return null;
+        }
+    }
+
+    static async isValidWord(word) {
+        if (this.isDemoMode) {
+            return Promise.resolve(true);
+        } else {
+            const response = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + word.toLowerCase());
+            if (Array.isArray(response.data)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    static submitAnswer(sessionId, answer) {
+        return this.isValidWord(answer).then(isValid => {
+            if (!isValid) {
+                return Promise.reject(false);
+            } else {
+                const output = [];
+                const word = this.getWordFromSession(sessionId);
+                if (word === null) {
+                    return Promise.reject(false);
+                }
+                for (let i = 0; i < word.length; i++) {
+                    const character = word[i];
+                    if (character === answer[i]) {
+                        output[i] = "correct";
+                    } else {
+                        output[i] = "absent";
+                    }
+
+                    if (output[i] !== "correct" && word.includes(answer[i])) {
+                        output[i] = "present";
+                    }
+                }
+                return Promise.resolve(Object.values(output));
+            }
+        });
+    }
+}
+
+module.exports = GameEngine;
