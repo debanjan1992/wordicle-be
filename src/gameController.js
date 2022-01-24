391f96e8111b5f53d15771af9c19ee4aee08fc89
Debanjan Saha <debanjansaha1992@gmail.com>
2022-01-24 09:42:45 +0530

    changed APIs

--- src/gameController.js
+++ src/gameController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const GameEngine = require("./services/gameEngine");
 const router = express.Router();
 
-router.get("/newGame", GameEngine.startNewGame);
+router.post("/newGame", GameEngine.startNewGame);
 router.get("/session", GameEngine.getSessionDetails);
 router.post("/submit", GameEngine.submitWord);
 router.get("/reveal", GameEngine.revealWord);
